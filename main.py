cc1b82615b590144d54f965aea2156f54c9b6a96
Kostis Mavrogiorgos <63185922+mavrogiorgos@users.noreply.github.com>
2022-05-09 15:01:09 +0300

    Transform dataset and update item-based approach.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Authors: Ioanna Kandi & Konstantinos Mavrogiorgos
 """
 # code
 import numpy as np
@@ -14,43 +12,57 @@ import seaborn as sns
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-ratings = pd.read_csv("https://s3-us-west-2.amazonaws.com/recommender-tutorial/ratings.csv")
-ratings.head()
+ratings = pd.read_csv("C:/Users/Komav/Desktop/recSys/DATA_S2/q1-dataset/user_shows_new1.txt")
+#ratings.head()
+#ratings.insert(0, 'userId', range(1, 1 + len(ratings)))
+#ratings.to_csv('user_shows_new.txt', index=False)
 
-movies = pd.read_csv("https://s3-us-west-2.amazonaws.com/recommender-tutorial/movies.csv")
-movies.head()
+#transform the given dataset
+shows = pd.read_csv("C:/Users/Komav/Desktop/recSys/DATA_S2/q1-dataset/shows_new1.txt")
+#shows.head()
+#conversion of dataset columns to rows
+new_ratings=pd.melt(ratings,id_vars=["userId"], 
+        var_name="show", 
+        value_name="Watched")
+
+
+#new_ratings.to_csv('user_shows_new1.txt', index=False)
 
 n_ratings = len(ratings)
-n_movies = len(ratings['movieId'].unique())
+#n_shows = len(shows)
+n_shows = len(ratings['Show'].unique())
 n_users = len(ratings['userId'].unique())
 
 print(f"Number of ratings: {n_ratings}")
-print(f"Number of unique movieId's: {n_movies}")
+print(f"Number of unique Show's: {n_shows}")
 print(f"Number of unique users: {n_users}")
-print(f"Average ratings per user: {round(n_ratings/n_users, 2)}")
-print(f"Average ratings per movie: {round(n_ratings/n_movies, 2)}")
+#print(f"Average shows watched per user: {round(n_ratings/n_users, 2)}")
+#print(f"Average watched status per show: {round(n_ratings/n_shows, 2)}")
 
-user_freq = ratings[['userId', 'movieId']].groupby('userId').count().reset_index()
+
+user_freq = ratings[['userId', 'Show']].groupby('userId').count().reset_index()
 user_freq.columns = ['userId', 'n_ratings']
 user_freq.head()
 
 
-# Find Lowest and Highest rated movies:
-mean_rating = ratings.groupby('movieId')[['rating']].mean()
-# Lowest rated movies
-lowest_rated = mean_rating['rating'].idxmin()
-movies.loc[movies['movieId'] == lowest_rated]
-# Highest rated movies
-highest_rated = mean_rating['rating'].idxmax()
-movies.loc[movies['movieId'] == highest_rated]
-# show number of people who rated movies rated movie highest
-ratings[ratings['movieId']==highest_rated]
-# show number of people who rated movies rated movie lowest
-ratings[ratings['movieId']==lowest_rated]
-
-## the above movies has very low dataset. We will use bayesian average
-movie_stats = ratings.groupby('movieId')[['rating']].agg(['count', 'mean'])
-movie_stats.columns = movie_stats.columns.droplevel()
+# =============================================================================
+# # Find Lowest and Highest rated shows:
+# mean_rating = ratings.groupby('Show')[['rating']].mean()
+# # Lowest rated shows
+# lowest_rated = mean_rating['rating'].idxmin()
+# shows.loc[shows['Show'] == lowest_rated]
+# # Highest rated shows
+# highest_rated = mean_rating['rating'].idxmax()
+# shows.loc[shows['Show'] == highest_rated]
+# # show number of people who rated shows rated show highest
+# ratings[ratings['Show']==highest_rated]
+# # show number of people who rated shows rated show lowest
+# ratings[ratings['Show']==lowest_rated]
+# 
+# ## the above shows has very low dataset. We will use bayesian average
+# show_stats = ratings.groupby('Show')[['rating']].agg(['count', 'mean'])
+# show_stats.columns = show_stats.columns.droplevel()
+# =============================================================================
 
 # Now, we create user-item matrix using scipy csr matrix
 from scipy.sparse import csr_matrix
@@ -58,55 +70,53 @@ from scipy.sparse import csr_matrix
 def create_matrix(df):
 	
 	N = len(df['userId'].unique())
-	M = len(df['movieId'].unique())
+	M = len(df['Show'].unique())
 	
 	# Map Ids to indices
 	user_mapper = dict(zip(np.unique(df["userId"]), list(range(N))))
-	movie_mapper = dict(zip(np.unique(df["movieId"]), list(range(M))))
+	show_mapper = dict(zip(np.unique(df["Show"]), list(range(M))))
 	
 	# Map indices to IDs
 	user_inv_mapper = dict(zip(list(range(N)), np.unique(df["userId"])))
-	movie_inv_mapper = dict(zip(list(range(M)), np.unique(df["movieId"])))
+	show_inv_mapper = dict(zip(list(range(M)), np.unique(df["Show"])))
 	
 	user_index = [user_mapper[i] for i in df['userId']]
-	movie_index = [movie_mapper[i] for i in df['movieId']]
+	show_index = [show_mapper[i] for i in df['Show']]
 
-	X = csr_matrix((df["rating"], (movie_index, user_index)), shape=(M, N))
+	X = csr_matrix((df["Watched"], (show_index, user_index)), shape=(M, N))
 	
-	return X, user_mapper, movie_mapper, user_inv_mapper, movie_inv_mapper
+	return X, user_mapper, show_mapper, user_inv_mapper, show_inv_mapper
 
-X, user_mapper, movie_mapper, user_inv_mapper, movie_inv_mapper = create_matrix(ratings)
+X, user_mapper, show_mapper, user_inv_mapper, show_inv_mapper = create_matrix(ratings)
 
 from sklearn.neighbors import NearestNeighbors
 """
-Find similar movies using KNN
+Find similar shows using KNN
 """
-def find_similar_movies(movie_id, X, k, metric='cosine', show_distance=False):
+def find_similar_shows(show_title, X, k, metric='cosine', show_distance=False):
 	
 	neighbour_ids = []
 	
-	movie_ind = movie_mapper[movie_id]
-	movie_vec = X[movie_ind]
+	show_ind = show_mapper[show_title]
+	show_vec = X[show_ind]
 	k+=1
 	kNN = NearestNeighbors(n_neighbors=k, algorithm="brute", metric=metric)
 	kNN.fit(X)
-	movie_vec = movie_vec.reshape(1,-1)
-	neighbour = kNN.kneighbors(movie_vec, return_distance=show_distance)
+	show_vec = show_vec.reshape(1,-1)
+	neighbour = kNN.kneighbors(show_vec, return_distance=show_distance)
 	for i in range(0,k):
 		n = neighbour.item(i)
-		neighbour_ids.append(movie_inv_mapper[n])
+		neighbour_ids.append(show_inv_mapper[n])
 	neighbour_ids.pop(0)
 	return neighbour_ids
 
 
-movie_titles = dict(zip(movies['movieId'], movies['title']))
+shows_titles = shows['Show']
 
-movie_id = 3
+show_title = 'The Situation Room with Wolf Blitzer'
 
-similar_ids = find_similar_movies(movie_id, X, k=10)
-movie_title = movie_titles[movie_id]
+similar_shows = find_similar_shows(show_title, X, k=10)
 
-print(f"Since you watched {movie_title}")
-for i in similar_ids:
-	print(movie_titles[i])
+print(f"Since you watched {show_title}")
+print(similar_shows)
 
